5e982e93d56dcf64cc2f830cab0be2b95218c623
siniseong <shinhuiseong07@gmail.com>
2024-12-16 09:25:09 +0900

    DOCS :: 푸터 내용 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 text-center py-4 mt-4 sm:mt-8">
       <p className="text-xs sm:text-base md:text-lg text-gray-600">
-        © 2024 siniseong. All rights reserved.
+      🚀 프로젝트의 <b><a href="https://github.com/siniseong/suuk" className="text-blue-500">레포지토리</a></b>와 <b><a href="https://siniseong.notion.site/SSUK-149e00310b39806aba20fc5bf3657cc3?pvs=4" className="text-blue-500">노션</a></b>을 구경해보세요.
       </p>
     </footer>
   );
